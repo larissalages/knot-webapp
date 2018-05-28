53a28df85d59f9d1c509a5d2392058a705f2f925
Larissa <llo@cin.ufpe.br>
2018-05-28 14:35:16 -0300

    fix interface

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import {
   Panel,
+  Button,
+  Table,
   FormControl,
   FormGroup,
   ControlLabel,
   Form,
-  Col,
-  Table,
-  Button,
-  Image,
-  Row
+  Col
 } from "react-bootstrap";
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:3003');
 
 class App extends Component {
   constructor(props) {
@@ -42,10 +38,8 @@ class App extends Component {
         response: ""
       },
       getData: { thingUuid: "", itemId: "", response: "" },
-      readData: { thingUuid: "", response: "" },
       getDevices: { gateway: "", response: "" },
-      subscribe: { thingUuid: "", response: "" },
-      isOn: false,
+      subscribe: { thingUuid: "", response: "" }
     };
     this._onChangeSetConfig = this._onChangeSetConfig.bind(this);
     this.setConfig = this.setConfig.bind(this);
@@ -55,11 +49,9 @@ class App extends Component {
     this.getData = this.getData.bind(this);
     this._onChangeGetDevices = this._onChangeGetDevices.bind(this);
     this.getDevices = this.getDevices.bind(this);
-    this._onChangeDefaultConfigs = this._onChangeDefaultConfigs.bind(this);
     this._onChangeSubscribe = this._onChangeSubscribe.bind(this);
     this.subscribe = this.subscribe.bind(this);
-    this._onChangeReadData = this._onChangeReadData.bind(this);
-    this.readData = this.readData.bind(this);
+    this._onChangeDefaultConfigs = this._onChangeDefaultConfigs.bind(this);
   }
 
   _onChangeSetConfig = function(e) {
@@ -83,20 +75,15 @@ class App extends Component {
     getDevices[e.target.name] = e.target.value;
     this.setState({ getDevices: getDevices });
   };
-  _onChangeDefaultConfigs = function(e) {
-    const defaultConfigs = this.state.defaultConfigs;
-    defaultConfigs[e.target.name] = e.target.value;
-    this.setState({ defaultConfigs: defaultConfigs });
-  };
   _onChangeSubscribe = function(e) {
     const subscribe = this.state.subscribe;
     subscribe[e.target.name] = e.target.value;
     this.setState({ subscribe: subscribe });
   };
-  _onChangeReadData = function(e) {
-    const readData = this.state.readData;
-    readData[e.target.name] = e.target.value;
-    this.setState({ readData: readData });
+  _onChangeDefaultConfigs = function(e) {
+    const defaultConfigs = this.state.defaultConfigs;
+    defaultConfigs[e.target.name] = e.target.value;
+    this.setState({ defaultConfigs: defaultConfigs });
   };
   setConfig = function(e) {
     const setConfig = this.state.setConfig;
@@ -171,7 +158,7 @@ class App extends Component {
       thingUuid: this.state.getData.thingUuid,
       itemId: this.state.getData.itemId
     };
-    fetch("/httpGetData", {
+    fetch("/getData", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -197,7 +184,7 @@ class App extends Component {
       ownerToken: this.state.defaultConfigs.ownerToken,
       hostname: this.state.defaultConfigs.hostname,
       port: this.state.defaultConfigs.port,
-      gateway: this.state.defaultConfigs.ownerUuid
+      gateway: this.state.getDevices.gateway
     };
     fetch("/getDevices", {
       method: "POST",
@@ -210,28 +197,8 @@ class App extends Component {
       .then(res => res.json())
       .then(
         function(json) {
-          var resp = [];
-          //Websockets
-          for (var i in json) {
-            var device = json[i];
-            resp[i] = {
-              uuid : device.uuid,
-              online : device.online,
-              name : device.name,
-              schema : device.schema
-            }
-          }
-          /* HTTP
-          for (var i in json.body.devices) {
-            var device = json.body.devices[i];
-            resp[i] = {
-              uuid : device.uuid,
-              online : device.online,
-              name : device.name,
-              schema : device.schema
-            }
-          }*/
-          getDevices["response"] = JSON.stringify(resp, null, 3);
+          getDevices["response"] = JSON.stringify(json, null, 3);
+          console.log(json);
           this.setState({ getDevices: getDevices });
         }.bind(this)
       );
@@ -245,38 +212,9 @@ class App extends Component {
       ownerToken: this.state.defaultConfigs.ownerToken,
       hostname: this.state.defaultConfigs.hostname,
       port: this.state.defaultConfigs.port,
-      thingUuid: subscribe.thingUuid
+      thingUuid: this.state.subscribe.thingUuid
     };
-    socket.on(subscribe.thingUuid, function(response) {
-      response = JSON.parse(response);
-      response = {
-        value: response.payload.value,
-        timestamp : response.payload.timestamp
-      }
-      subscribe.response = JSON.stringify(response, null, 3);
-
-      if(response.value === "false")
-          this.setState({ isOn: false });
-      if(response.value === "true")
-          this.setState({ isOn: true });
-
-      this.setState({ subscribe: subscribe });
-    }.bind(this));
-    socket.emit('httpSubscribe', request);
-    e.preventDefault();
-  };
-
-  readData = function(e) {
-    const readData = this.state.readData;
-    const request = {
-      ownerUuid: this.state.defaultConfigs.ownerUuid,
-      ownerToken: this.state.defaultConfigs.ownerToken,
-      hostname: this.state.defaultConfigs.hostname,
-      port: this.state.defaultConfigs.port,
-      thingUuid: this.state.readData.thingUuid,
-      itemId: this.state.readData.itemId
-    };
-    fetch("/httpReadData", {
+    fetch("/subscribe", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -287,22 +225,15 @@ class App extends Component {
       .then(res => res.json())
       .then(
         function(json) {
-          readData["response"] = JSON.stringify(json, null, 3);
+          subscribe["response"] = JSON.stringify(json, null, 3);
           console.log(json);
-          this.setState({ readData: readData });
+          this.setState({ subscribe: subscribe });
         }.bind(this)
       );
     e.preventDefault();
   };
 
   render() {
-    const isOn = this.state.isOn;
-    let image = null;
-    if (isOn) {
-      image = <Image src={require('./images/on.png')} rounded />;
-    } else {
-      image = <Image src={require('./images/off.png')} rounded />;
-    }
     return (
       <div className="App">
         <h1>KNoT Sample App</h1>
@@ -334,9 +265,35 @@ class App extends Component {
                 />
               </Col>
             </FormGroup>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Hostname
+              </Col>
+              <Col sm={10}>
+                <FormControl
+                  type="text"
+                  name="hostname"
+                  value={this.state.defaultConfigs.hostname}
+                  onChange={this._onChangeDefaultConfigs}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Owner UUID
+              </Col>
+              <Col sm={10}>
+                <FormControl
+                  type="text"
+                  name="port"
+                  value={this.state.defaultConfigs.port}
+                  onChange={this._onChangeDefaultConfigs}
+                />
+              </Col>
+            </FormGroup>
           </Form>
         </Panel>
-        <Panel key={1} collapsible header="Set Config">
+        <Panel key={1} header="Set Config">
           <form>
             <Table responsive>
               <thead>
@@ -430,17 +387,7 @@ class App extends Component {
             <p>{this.state.setConfig.response}</p>
           </Panel>
         </Panel>
-        <Panel key={2} collapsible header="Get Devices">
-            <Button bsStyle="primary" onClick={this.getDevices}>
-              Get Devices
-            </Button>
-            <br/>
-          <b>Get Devices Response:</b>
-          <Panel>
-            <p>{this.state.getDevices.response}</p>
-          </Panel>
-        </Panel>
-        <Panel key={3} collapsible header="Set Data">
+        <Panel key={2} header="Set Data">
           <form>
             <Table responsive>
               <thead>
@@ -495,17 +442,90 @@ class App extends Component {
           </form>
           <b>Set Data Response:</b>
           <Panel>
-            <Row>
-              <Col xs={12} md={8}>
-                <p>{this.state.setData.response}</p>
-              </Col>
-              <Col xs={6} md={4}>
-                {image}
-              </Col>
-            </Row>
+            <p>{this.state.setData.response}</p>
           </Panel>
         </Panel>
-        <Panel key={4} collapsible header="Subscribe">
+        <Panel key={3} header="Get Data">
+          <form>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Parameter</th>
+                  <th>Value</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Thing UUID</td>
+                  <td>
+                    <FormControl
+                      type="text"
+                      value={this.state.getData.thingUuid}
+                      name="thingUuid"
+                      onChange={this._onChangeGetData}
+                    />
+                  </td>
+                  <td>Thing UUID</td>
+                </tr>
+                <tr>
+                  <td>Item ID</td>
+                  <td>
+                    <FormControl
+                      type="text"
+                      value={this.state.getData.itemId}
+                      name="itemId"
+                      onChange={this._onChangeGetData}
+                    />
+                  </td>
+                  <td>Id for the item to apply config</td>
+                </tr>
+              </tbody>
+            </Table>
+            <Button bsStyle="primary" onClick={this.getData}>
+              Get Data
+            </Button>
+          </form>
+          <b>Get Data Response:</b>
+          <Panel>
+            <p>{this.state.getData.response}</p>
+          </Panel>
+        </Panel>
+        <Panel key={4} header="Get Devices">
+          <form>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Parameter</th>
+                  <th>Value</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Gateway</td>
+                  <td>
+                    <FormControl
+                      type="text"
+                      value={this.state.getDevices.gateway}
+                      name="gateway"
+                      onChange={this._onChangeGetDevices}
+                    />
+                  </td>
+                  <td>Gateway</td>
+                </tr>
+              </tbody>
+            </Table>
+            <Button bsStyle="primary" onClick={this.getDevices}>
+              Get Devices
+            </Button>
+          </form>
+          <b>Get Devices Response:</b>
+          <Panel>
+            <p>{this.state.getDevices.response}</p>
+          </Panel>
+        </Panel>
+        <Panel key={5} header="Subscribe">
           <form>
             <Table responsive>
               <thead>
@@ -535,42 +555,8 @@ class App extends Component {
             </Button>
           </form>
           <b>Subscribe Response:</b>
-          <Panel className="ScrollStyle">
-              <p>{this.state.subscribe.response}</p>
-          </Panel>
-        </Panel>
-        <Panel key={3} collapsible header="Read Data">
-          <form>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Parameter</th>
-                  <th>Value</th>
-                  <th>Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Thing UUID</td>
-                  <td>
-                    <FormControl
-                      type="text"
-                      value={this.state.readData.thingUuid}
-                      name="thingUuid"
-                      onChange={this._onChangeReadData}
-                    />
-                  </td>
-                  <td>Thing UUID</td>
-                </tr>
-              </tbody>
-            </Table>
-            <Button bsStyle="primary" onClick={this.readData}>
-              Read Data
-            </Button>
-          </form>
-          <b>Read Data Response:</b>
           <Panel>
-                <p>{this.state.readData.response}</p>
+            <p>{this.state.subscribe.response}</p>
           </Panel>
         </Panel>
       </div>
